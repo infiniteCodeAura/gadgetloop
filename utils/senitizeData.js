b3edffdeeea37ba3712a7c1d99b4c7ef0c4824ff
anonyTheCoder <s.g.devil.88@gmail.com>
2025-05-17 17:57:07 +0545

    update

--- utils/senitizeData.js
+++ utils/senitizeData.js
@@ -1,9 +1,27 @@
 import sanitizeHtml from "sanitize-html";
+import validator from "validator"
 export const sanitizeData = (input)=>{
     let data = input?.trim()|| "";
-    data = validateForgotPasswordData.escape(data);
+    data = validator.escape(data);
     data = sanitizeHtml(data,{
         allowedTags: [],
         allowedAttributes: {},
     })
+    return data
 }
+
+export const emailSenitize = (input)=>{
+    let rawEmail = input?.toString().trim() || "";
+    //sanitize email 
+    let email = sanitizeHtml(rawEmail,{
+        allowedTags: [],
+        allowedAttributes: {},
+    })
+    //email validator 
+    email = validator.normalizeEmail(email);
+
+    return email;
+
+}
+
+
